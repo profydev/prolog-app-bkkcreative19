54883b8e8b51c4443f73902e71e14141ff20bdd7
Baron King <bkkcreative16@gmail.com>
2022-12-04 09:49:04 -0600

    Added error message on error

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import styled from "styled-components";
 import { CSSProperties } from "react";
 import { ProjectCard } from "../project-card";
 import { useProjects } from "../../api/use-projects";
-import { breakpoint, space } from "@styles/theme";
+import { breakpoint, space, color, textFont } from "@styles/theme";
 import ClipLoader from "react-spinners/ClipLoader";
 
 const List = styled.ul`
@@ -20,10 +21,32 @@ const List = styled.ul`
   }
 `;
 
+const ErrorContainer = styled.div`
+  background: ${color("error", 25)};
+  border: 1px solid ${color("error", 300)};
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  padding: ${space(1, 4)};
+  ${textFont("md", "medium")}
+`;
+
+const ErrorMessage = styled.p`
+  color: ${color("error", 700)};
+  margin-left: ${space(3)};
+`;
+
+const TryAgain = styled.div`
+  color: ${color("error", 700)};
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  gap: 8px;
+`;
+
 const override: CSSProperties = {
   display: "block",
   margin: "129px auto",
-  // borderColor: "#7F56D9",
   color: "#7F56D9",
 };
 
@@ -35,17 +58,25 @@ export function ProjectList() {
       <ClipLoader
         cssOverride={override}
         size={58}
-        // aria-label="Loading Spinner"
+        aria-label="Loading Spinner"
       />
     );
   }
 
   if (isError) {
-    console.error(error);
-    return <div>Error: {error.message}</div>;
+    return (
+      <ErrorContainer>
+        <img src="/icons/error.svg" alt="error" />
+        <ErrorMessage>
+          There was a problem while loading the project data
+        </ErrorMessage>
+        <TryAgain>
+          Try again
+          <img src="/icons/arrow-right.svg" alt="right-arrow" />
+        </TryAgain>
+      </ErrorContainer>
+    );
   }
-
-  console.log(data);
 
   return (
     <List>
