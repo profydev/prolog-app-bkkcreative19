6c60a4cd65109dc1a0dedbea25ffc4c38ed54a55
Baron King <59720532+bkkcreative19@users.noreply.github.com>
2022-12-12 13:37:28 -0600

    UI kit input (#17)
    * Sidebar Nav Arrow now points right when collapsed
    
    * added input

--- features/ui/checkbox/checkboxContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import styled, { css } from "styled-components";
-
-export function CheckboxContainer() {
-  const [checked, setIsChecked] = useState();
-  return <div></div>;
-}
